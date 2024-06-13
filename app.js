84cb67ff2a794c18cd7e817541d313693307ff73
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-13 19:54:00 +0200

    calculateTotal func add

--- app.js
+++ app.js
@@ -15,52 +15,111 @@ const movType = document.querySelector(".movements-type");
 const movDescription = document.querySelector(".movements-description");
 const movAmount = document.querySelector(".movements-amount");
 
-const createIncomeRow = () => {
-  const movRow = document.createElement("div");
-  movRow.className = "movements-row d-flex justify-content-around p-2";
+//*!
+const sumIn = document.querySelector(".total-in-value");
+const sumOut = document.querySelector(".total-out-value");
+const sumTotal = document.querySelector(".total-value");
 
-  movRow.innerHTML = `
-        <div class="movements-date">${`${new Date().getDay()}.${new Date().getMonth()}.${new Date().getFullYear()}`}</div>
-        <div class="movements--type movements--income"> Income
-        </div>
-        <div class="movements-description">${descriptionIncome.value}</div>
-        <div class="movements-amount">${amountIncome.value} €</div>
-  `;
+//!
+let incomes = JSON.parse(localStorage.getItem("incomes")) || [];
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+//!
 
-  movContainer.appendChild(movRow);
+const displayMovements = () => {
+  movContainer.innerHTML = "";
+
+  incomes.forEach((income) => {
+    const movRow = document.createElement("div");
+    movRow.className = "movements-row d-flex justify-content-around p-2";
+
+    movRow.innerHTML = `
+          <div class="movements-date">${new Date(
+            income.id
+          ).toLocaleDateString()}</div>
+          <div class="movements--type movements--income">Income</div>
+          <div class="movements-description">${income.description}</div>
+          <div class="movements-amount">${income.amount} €</div>
+    `;
+    movContainer.appendChild(movRow);
+  });
+
+  expenses.forEach((expense) => {
+    const movRow = document.createElement("div");
+    movRow.className = "movements-row d-flex justify-content-around p-2";
+
+    movRow.innerHTML = `
+          <div class="movements-date">${new Date(
+            expense.id
+          ).toLocaleDateString()}</div>
+          <div class="movements--type movements--outcome">Expense</div>
+          <div class="movements-description">${expense.description}</div>
+          <div class="movements-amount">${expense.amount} €</div>
+    `;
+    movContainer.appendChild(movRow);
+  });
 };
 
-const createExpenseRow = () => {
-  const movRow = document.createElement("div");
-  movRow.className = "movements-row d-flex justify-content-around p-2";
+const addIncome = () => {
+  if (
+    amountIncome.value !== "" &&
+    amountIncome.value > 0 &&
+    descriptionIncome.value !== ""
+  ) {
+    const newIncome = {
+      description: descriptionIncome.value,
+      amount: amountIncome.value,
+      id: new Date().getTime(),
+    };
+    incomes.push(newIncome);
+    localStorage.setItem("incomes", JSON.stringify(incomes));
+    displayMovements();
+    amountIncome.value = "";
+    descriptionIncome.value = "";
+  }
+};
 
-  movRow.innerHTML = `
-        <div class="movements-date">${`${new Date().getDay()}.${
-          new Date().getMonth() + 1
-        }.${new Date().getFullYear()}`}</div>
-        <div class="movements--type movements--outcome"> Expense
-        </div>
-        <div class="movements-description">${descriptionExpense.value}</div>
-        <div class="movements-amount">${amountExpense.value} €</div>
-  `;
-
-  movContainer.appendChild(movRow);
+const addExpense = () => {
+  if (
+    amountExpense.value !== "" &&
+    amountExpense.value > 0 &&
+    descriptionExpense.value !== ""
+  ) {
+    const newExpense = {
+      description: descriptionExpense.value,
+      amount: amountExpense.value,
+      id: new Date().getTime(),
+    };
+    expenses.push(newExpense);
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+    displayMovements();
+    amountExpense.value = "";
+    descriptionExpense.value = "";
+  }
 };
 
 btnIncome.addEventListener("click", (e) => {
   e.preventDefault();
-  if (amountIncome.value !== "" && descriptionIncome.value !== "") {
-    createIncomeRow();
-    amountIncome.value = "";
-    descriptionIncome.value = "";
-  }
+  addIncome();
+  calculateTotal();
 });
 
 btnExpense.addEventListener("click", (e) => {
   e.preventDefault();
-  if (amountExpense.value !== "" && descriptionExpense.value !== "") {
-    createExpenseRow();
-    amountExpense.value = "";
-    descriptionExpense.value = "";
-  }
+  addExpense();
+  calculateTotal();
 });
+
+const calculateTotal = () => {
+  sumIn.textContent = incomes.reduce(
+    (sum, { amount }) => sum + Number(amount),
+    0
+  );
+  sumOut.textContent = expenses.reduce(
+    (sum, { amount }) => sum + Number(amount),
+    0
+  );
+  sumTotal.textContent = sumIn.textContent + sumOut.textContent;
+};
+
+displayMovements();
+calculateTotal();
